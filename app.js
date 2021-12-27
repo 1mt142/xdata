f505368ed120c6b0d325f2352d957c1c03e9cc5b
jsking-bd <imtiaz.ucb@gmail.com>
2021-12-28 00:23:05 +0600

    Parser

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 // const http = require("http");
 const express = require("express");
+const bodyParser = require('body-parser')
 const port = 5000;
 const app = express();
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use("/add-product", (req, res, next) => {
-  console.log("This is Middleware");
-  res.send("Add to Product");
+  // console.log("This is Middleware");
+  
+  // res.send("Add to Product");
+  res.send('<form action="/product" method="POST"><input type="text" name="title"> <button type="submit">+</button></form>');
+
   // Next is used for -> next instruction
   // next();
 });
+app.post('/product',(req,res,next)=>{
+  console.log(req.body);
+   res.redirect('/')
+});
+
 app.use("/", (req, res, next) => {
   console.log("This is another Middleware");
-  res.send("Hello World!");
+  res.send('Home Page')
 });
 // Output Web
 // End Output Web
@@ -19,6 +29,8 @@ app.use("/", (req, res, next) => {
 // server.listen(port, () => {
 //   console.log(`http://localhost:${port}`);
 // });
+
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
